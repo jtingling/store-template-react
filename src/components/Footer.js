744beabd71249944638eafffd637aa368d12ccb5
Jacob T <jacob.tingling@protonmail.com>
2021-02-03 16:49:59 -0500

    added responsive footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,41 +6,67 @@ const Footer = () => {
     return (
         <div className='footer-container'>
             <footer>
-                <div className='footer-about'>
-                    <img src={logo} alt={logo} className='logo-footer'></img>
-                    <p className='footer-text'>
-                        Bibisama is an Otaku clothing brand that blends the best of the culture and art style with unique clothing designs you won't find anywhere else.
-                        engo@bibisama.com
-                        © 2021 Bibisama Apparel Powered by Japanese Streetwear
-                </p>
+                <div>
+                    <div className='footer-about'>
+                        <div className='logo-container'>
+                            <img src={logo} alt={logo} className='logo-footer'></img>
+                        </div>
+                        <div className='footer-text-container'>
+                            <p className='footer-text'>
+                                Bibisama is an Otaku clothing brand that blends the best of the culture and art style with unique
+                                clothing designs you won't find anywhere else.<br/><br/>
+                                <span id='email'>Contact Us: engo@bibisama.com</span><br/><br/>
+                                <span id='copyright'>© 2021 Bibisama Apparel Powered by Japanese Streetwear</span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div className='link-table'>
-                    <table>
-                        <tr>
-                            <th>SHOP</th>
-                            <th>BIBISAMA</th>
-                        </tr>
-                        <tr>
-                            <td>Landing</td>
-                            <td>Support</td>
-                        </tr>
-                        <tr>
-                            <td>Shirts</td>
-                            <td>About Us</td>
-                        </tr>
-                        <tr>
-                            <td>Jacket + Sweaters</td>
-                            <td>Store Policies</td>
-                        </tr>
-                        <tr>
-                            <td>Accessories</td>
-                            <td>Upcoming Events</td>
-                        </tr>
-                        <tr>
-                            <td>Search</td>
-                            <td>SiteMap</td>
-                        </tr>
-                    </table>
+                    <div className='shop-table'>
+                        <table>
+                            <tr>
+                                <th>SHOP</th>
+                            </tr>
+                            <tr>
+                                <td>Landing</td>
+                            </tr>
+                            <tr>
+                                <td>Shirts</td>
+                            </tr>
+                            <tr>
+                                <td>Jacket + Sweaters</td>
+                            </tr>
+                            <tr>
+                                <td>Accessories</td>
+                            </tr>
+                            <tr>
+                                <td>Search</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div className='store-table'>
+                        <table>
+                            <tr>
+
+                                <th>BIBISAMA</th>
+                            </tr>
+                            <tr>
+                                <td>Support</td>
+                            </tr>
+                            <tr>
+                                <td>About Us</td>
+                            </tr>
+                            <tr>
+                                <td>Store Policies</td>
+                            </tr>
+                            <tr>
+                                <td>Upcoming Events</td>
+                            </tr>
+                            <tr>
+                                <td>SiteMap</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
                 <div className='social-media'>
                     <ul className='inline-list'>
