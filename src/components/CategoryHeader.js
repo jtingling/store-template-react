21b383ed5a998152e29bae640b4616e3a24f42ba
Jacob T <jacob.tingling@protonmail.com>
2021-03-18 17:00:35 -0400

    added responsive css styling

--- src/components/CategoryHeader.js
+++ src/components/CategoryHeader.js
@@ -6,9 +6,7 @@ import '../css/categoryHeader.css';
 const CategoryHeader = (props) => {
     let context = useContext(CheckoutContext);
     const toggleNavLinks = () => {
-        if (window.innerWidth < 768 && props.className === "nav-header") {
-            return <></>
-        } else {
+        if (window.innerWidth < 768 && props.className === "nav-list") {
             return (
                 <nav className={props.className}>
                     <Link to="/shoes"><h5 onClick={() => context.toggleMenu()} className='nav-links'>Shirts</h5></Link>
@@ -16,6 +14,16 @@ const CategoryHeader = (props) => {
                     <Link to="/accessories"><h5 onClick={() => context.toggleMenu()} className='nav-links'>Accessories</h5></Link>
                     <Link to="/support"><h5 onClick={() => context.toggleMenu()} className='nav-links'>Support</h5></Link>
                     <Link to="/aboutus"><h5 onClick={() => context.toggleMenu()} className='nav-links'>About Us</h5></Link>
+                </nav>
+            )
+        } else {
+            return (
+                <nav className={props.className}>
+                    <Link to="/shoes"><h5 className='nav-links'>Shirts</h5></Link>
+                    <Link to="/painting"><h5 className='nav-links'>Jacket + Sweaters</h5></Link>
+                    <Link to="/accessories"><h5 className='nav-links'>Accessories</h5></Link>
+                    <Link to="/support"><h5 className='nav-links'>Support</h5></Link>
+                    <Link to="/aboutus"><h5 className='nav-links'>About Us</h5></Link>
                 </nav>
             )
         }
--- src/components/Products/ProductDetailModal.js
+++ src/components/Products/ProductDetailModal.js
@@ -2,15 +2,15 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CheckoutContext } from '../../App'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faAngleDown, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import ImageGallary from 'react-image-gallery';
 import '../../css/productDetail.css'
 
 const ProductDetailModal = (props) => {
-    const checkout = useContext(CheckoutContext);
-    const [variantId, setVariantId] = useState(0);
-    const [isHidden, setHidden] = useState([]);
     const { productData } = props.location.state;
+    const checkout = useContext(CheckoutContext);
+    const [variantId, setVariantId] = useState(productData.variants.edges[0].node.id);
+    const [isHidden, setHidden] = useState([]);
 
     const getImages = () => {
         let images = [];
@@ -32,120 +32,126 @@ const ProductDetailModal = (props) => {
             throw new Error("no product");
         }
     }
-    useEffect(()=> {
+    useEffect(() => {
         setHidden([true, true, true, true])
-    },[])
+    }, [])
     if (productData === undefined) {
         return <h1>Loading Data...</h1>
     } else {
         return (
-            <article className='modal-window'>
-                <div className='product-images-container'>
-                    <ImageGallary
-                        items={getImages()}
-                        showNav={false}
-                        showBullets={true}
-                        showThumbnails={false}
-                        showFullscreenButton={false}
-                        useBrowserFullscreen={false}
-                        autoPlay={true}
-                        showPlayButton={false} />
-                </div>
-                <div className='product-description-container'>
-                    <div className='product-title'>
-                        <h1>{productData.handle}</h1>
-                        <div>
-                            <h2>${productData.variants.edges[0].node.price}</h2>
-                            <p><Link to='/support'>Shipping</Link> calculated at checkout</p>
+            <>
+                <article className='modal-window'>
+                    {console.log(productData)}
+                    <div className='product-images-container'>
+                        <ImageGallary
+                            items={getImages()}
+                            showNav={false}
+                            showBullets={true}
+                            showThumbnails={false}
+                            showFullscreenButton={false}
+                            useBrowserFullscreen={false}
+                            autoPlay={true}
+                            showPlayButton={false} />
+                    </div>
+                    <div className='product-description-container'>
+                        <div className='product-title'>
+                            <h1>{productData.handle}</h1>
+                            <div>
+                                <h2>${productData.variants.edges[0].node.price}</h2>
+                                <p><Link to='/support'>Shipping</Link> calculated at checkout</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='sizing-container'>
-                        <h3 className='sizing-title'>Size</h3>
-                        <div className='sizing-options'>
-                            {
-                                productData.options[0].values.map((size, idx) => {
-                                    return <button type='button' onClick={() => variantSelection(idx)}>{size}</button>
-                                })
-                            }
-                            <div></div>
+                        <div className='sizing-container'>
+                            <h3 className='sizing-title'>Size</h3>
+                            <div className='sizing-options'>
+                                {
+                                    productData.options[0].values.map((size, idx) => {
+                                        return <button type='button' className='size-buttons' onClick={() => variantSelection(idx)}>{size}</button>
+                                    })
+                                }
+                                <div></div>
+                            </div>
                         </div>
-                    </div>
-                    <div className='product-button-container'>
-                        <button type='button' onClick={() => checkout.addItemToCart(1, variantId)}>Add to Cart</button>
-                        <button type='button' onClick={() => checkout.buySingleItem(variantId)}>Buy It Now</button>
-                    </div>
-                    <div className='dropdown-container'>
-                        <button type='button' className='dropdown' onClick={() => setHidden([!isHidden[0], isHidden[1], isHidden[2], isHidden[3]])}>
-                            Description
+                        <div className='product-button-container'>
+                            <div><button type='button' className='product-addToCart' onClick={() => checkout.addItemToCart(1, variantId)}>ADD TO CART</button></div>
+                            <div><button type='button' className='product-buyNow' onClick={() => checkout.buySingleItem(variantId)}>BUY IT NOW</button></div>
+                        </div>
+                        <div className='dropdown-container'>
+                            <button type='button' className='dropdown' onClick={() => setHidden([!isHidden[0], isHidden[1], isHidden[2], isHidden[3]])}>
+                                Description
                             <span ><FontAwesomeIcon icon={isHidden[0] ? faAngleDown : faAngleUp} /></span>
-                        </button>
-                        {
-                            isHidden[0] ? <div id='description' hidden><p >{productData.descriptionHtml}</p></div> :
-                                <div id='description' ><p >{productData.descriptionHtml}</p></div>
-                        }
+                            </button>
+                            {
+                                isHidden[0] ? <div id='description' hidden><p >{productData.descriptionHtml}</p></div> :
+                                    <div id='description' ><p >{productData.descriptionHtml}</p></div>
+                            }
 
-                        <button type='button' className='dropdown' onClick={() => setHidden([isHidden[0], !isHidden[1], isHidden[2], isHidden[3]])}>
-                            Sizing Chart
+                            <button type='button' className='dropdown' onClick={() => setHidden([isHidden[0], !isHidden[1], isHidden[2], isHidden[3]])}>
+                                Sizing Chart
                             <span><FontAwesomeIcon icon={isHidden[1] ? faAngleDown : faAngleUp} /></span>
-                        </button>
-                        {
-                            isHidden[1] ? <code hidden>Sizing chart image goes here</code> : <code>Sizing chart image goes here</code>
-                        }
-                        <button type='button' className='dropdown' onClick={() => setHidden([isHidden[0], isHidden[1], !isHidden[2], isHidden[3]])}>
-                            Care Instructions
+                            </button>
+                            {
+                                isHidden[1] ? <code hidden>Sizing chart image goes here</code> : <code>Sizing chart image goes here</code>
+                            }
+                            <button type='button' className='dropdown' onClick={() => setHidden([isHidden[0], isHidden[1], !isHidden[2], isHidden[3]])}>
+                                Care Instructions
                             <span><FontAwesomeIcon icon={isHidden[2] ? faAngleDown : faAngleUp} /></span>
-                        </button>
-                        {
-                            isHidden[2] ? 
-                                        <div hidden>
-                                            <ul>
-                                                <li>All of our shirts come preshrunk</li>
-                                                <li>Machine washable</li>
-                                                <li>Cold wash only</li>
-                                                <li>Tumble dry on low heat</li>
-                                                <li>Do not bleach</li>
-                                            </ul>
-                                        </div> 
+                            </button>
+                            {
+                                isHidden[2] ?
+                                    <div hidden>
+                                        <ul>
+                                            <li>All of our shirts come preshrunk</li>
+                                            <li>Machine washable</li>
+                                            <li>Cold wash only</li>
+                                            <li>Tumble dry on low heat</li>
+                                            <li>Do not bleach</li>
+                                        </ul>
+                                    </div>
                                     :
-                                        <div>
-                                            <ul>
-                                                <li>All of our shirts come preshrunk</li>
-                                                <li>Machine washable</li>
-                                                <li>Cold wash only</li>
-                                                <li>Tumble dry on low heat</li>
-                                                <li>Do not bleach</li>
-                                            </ul>
-                                        </div>
-                        }
-                        <button type='button' className='dropdown' onClick={() => setHidden([isHidden[0], isHidden[1], isHidden[2], !isHidden[3]])}>
-                            How This Was Made
+                                    <div>
+                                        <ul>
+                                            <li>All of our shirts come preshrunk</li>
+                                            <li>Machine washable</li>
+                                            <li>Cold wash only</li>
+                                            <li>Tumble dry on low heat</li>
+                                            <li>Do not bleach</li>
+                                        </ul>
+                                    </div>
+                            }
+                            <button type='button' className='dropdown' onClick={() => setHidden([isHidden[0], isHidden[1], isHidden[2], !isHidden[3]])}>
+                                How This Was Made
                             <span><FontAwesomeIcon icon={isHidden[3] ? faAngleDown : faAngleUp} /></span>
-                        </button>
-                        {
-                            isHidden[3] ? 
-                                        <div hidden>
-                                            <ul>
-                                                <li>T-shirts and clothing are made in-house</li>
-                                                <li>95% cotton, 5% spandex</li>
-                                                <li>Designed in-house</li>
-                                                <li>Produced in-house</li>
-                                                <li>No outsourcing</li>
-                                            </ul>
-                                        </div>
+                            </button>
+                            {
+                                isHidden[3] ?
+                                    <div hidden>
+                                        <ul>
+                                            <li>T-shirts and clothing are made in-house</li>
+                                            <li>95% cotton, 5% spandex</li>
+                                            <li>Designed in-house</li>
+                                            <li>Produced in-house</li>
+                                            <li>No outsourcing</li>
+                                        </ul>
+                                    </div>
                                     :
-                                        <div>
-                                            <ul>
-                                                <li>T-shirts and clothing are made in-house</li>
-                                                <li>95% cotton, 5% spandex</li>
-                                                <li>Designed in-house</li>
-                                                <li>Produced in-house</li>
-                                                <li>No outsourcing</li>
-                                            </ul>
-                                        </div>
-                        }
+                                    <div>
+                                        <ul>
+                                            <li>T-shirts and clothing are made in-house</li>
+                                            <li>95% cotton, 5% spandex</li>
+                                            <li>Designed in-house</li>
+                                            <li>Produced in-house</li>
+                                            <li>No outsourcing</li>
+                                        </ul>
+                                    </div>
+                            }
+                        </div>
                     </div>
+                </article>
+                <div className='back-btn'>
+                    <button type='button'><Link to={productData.productType.toLowerCase()}><FontAwesomeIcon icon={faArrowLeft}/> Back to {productData.productType.toLowerCase()}</Link></button>
                 </div>
-            </article>
+            </>
         )
     }
 }
