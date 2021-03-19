76d376929e7f79461b513b97a3802057572d11fa
Jacob T <jacob.tingling@protonmail.com>
2021-03-19 16:54:48 -0400

    added responsive styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,36 +1,47 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import LineItem from './LineItem'
 import { Link } from 'react-router-dom';
 import { CheckoutContext } from '../App'
+import '../css/cart.css'
 
 const Cart = (props) => {
     let checkout = useContext(CheckoutContext);
+
+    useEffect(()=> {
+        window.scrollTo(0,0);
+    }, [])
 
     if (props.checkout === undefined) {
         return <h1>loading cart</h1>
     } else {
         return (
             <>
-                <section>
-                    <h2>Cart</h2>
+                {console.log(props.checkout)}
+                <section className="cart">
+                    <h1 className='cart-title'>CART</h1>
+                    <div className='cart-labels'>
+                        <h4></h4>
+                        <h4 id="cart-title-price">PRICE</h4>
+                        <h4 id="cart-title-quantity">QUANTITY</h4>
+                        <h4 id="cart-title-total">TOTAL</h4>
+                    </div>
                     <div className='cart-container'>
                         {
-                            props.checkout.cart.lineItems.length === 0 ? 
-                                <Link to='/'><button type='button' className='continue-shopping'>Continue Shopping</button></Link> :
-                                props.checkout.cart.lineItems.map((lineItem, idx) => {
-                                    return <LineItem key={idx} lineItem={lineItem} checkoutId={props.checkout.id}/>
-                                })
+                            props.checkout.cart.lineItems.map((lineItem, idx) => {
+                                return <LineItem key={idx} lineItem={lineItem} checkoutId={props.checkout.id} />
+                            })
                         }
                     </div>
                     <div className='subTotal'>
                         <div>
-                            <h4>SUBTOTAL</h4>
-                            <h4>{props.checkout.cart.subTotal}</h4>
-                            <p>Shipping, taxes, and discount codes calculated at checkout.</p>
+                        <h4>SUBTOTAL</h4>
+                            <h4>${props.checkout.cart.subTotal}</h4>
                         </div>
-                        <button type='button' onClick={() => checkout.openCheckout()}>Check Out</button>
+                            <p>Shipping, taxes, and discount codes calculated at checkout.</p>
+                            <button type='button' className='checkout-btn' onClick={() => checkout.openCheckout()}>Check Out</button>
+
+                        <Link to="/"><p >continue shopping</p></Link>
                     </div>
-                    <p>continue shopping</p>
                 </section>
             </>
         )
--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -1,5 +1,6 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { CheckoutContext } from '../App'
+import '../css/lineItem.css'
 
 const LineItem = (props) => {
     let checkout = useContext(CheckoutContext);
@@ -7,29 +8,37 @@ const LineItem = (props) => {
     return (
         <>
             <div className='cart-item'>
-                <div className='cart-info'>
-                    <p>{props.lineItem.title}</p>
-                </div>
-                <div className='cart-price'>
-
-                </div>
-                <div className='cart-quantity'>
-                    <div className='quantity'>
-                        <form>
-                            <label>Quantity:
-                                <input 
-                                    type='number' min='0' max='10' name="quantity" 
-                                    value={props.lineItem.quantity} 
-                                    onChange={(e)=>checkout.updateQuantity(e, props.lineItem)}/>
-                            </label>
-                        </form>
+                <div className="row-items">
+                    <div className='row-product'>
+                        <div className='row-product-img'>
+                            <img src={props.lineItem.variant.image.src} alt={props.lineItem.type} />
+                        </div>
+                        <div className='row-product-info'>
+                            <p>{props.lineItem.variant.sku}</p>
+                            <p className='row-variant-title'>{props.lineItem.variant.title}<p id="mobile-price">${props.lineItem.variant.price}</p></p>
+                        </div>
                     </div>
-                    {
-                        <button type='button' className='remove-btn' onClick={() => checkout.deleteItem(props.lineItem.id)}>remove</button>
-                    }
-                </div>
-                <div className='cart-item-total'>
-
+                    <div className='row-item'>
+                        <div className="row-item-price">
+                            ${props.lineItem.variant.price}
+                        </div>
+                        <div className='row-item-quantity'>
+                            <div className='quantity'>
+                                <form>
+                                <input
+                                            type='number' min='0' max='10' name="quantity"
+                                            value={props.lineItem.quantity}
+                                            onChange={(e) => checkout.updateQuantity(e, props.lineItem)} />
+                                </form>
+                            </div>
+                            {
+                                <button type='button' className='remove-btn' onClick={() => checkout.deleteItem(props.lineItem.id)}>remove</button>
+                            }
+                        </div>
+                        <div className='row-item-total'>
+                            <div>${(props.lineItem.quantity * parseInt(props.lineItem.variant.price, 10))}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
